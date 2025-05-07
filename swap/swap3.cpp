71eaf3d8ba51d263deedf9c1e193602639323cda
Mitchel Carson <mitchel.carson@gmail.com>
2025-05-07 19:52:06 -0400

    Added swap method that uses addresses as arguments.

---
+++ swap/swap3.cpp
@@ -0,0 +1,18 @@
+// Swap method that uses adresses as arguments
+#include <iostream>
+
+void swap(int & n, int & m);
+int main(){
+    int first = 0, second = 7;
+    swap(first, second);
+    std::cout << "first: " << first << ", second: " << second << std::endl;
+    std::cout << "Swap again: " << std::endl;
+    swap(first, second);
+    std::cout << "first: " << first << ", second: " << second << std::endl;
+}
+void swap(int & n, int & m){
+    int temp = n;
+    n = m;
+    m = temp;
+}
+// // The code above defines a function `swap` that takes two integer references as arguments and swaps the values they refer to.
